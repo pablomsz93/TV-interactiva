315118210dcca14eecc19f8df036355c4d28a0ae
“PABLO” <pablomanuel93@hotmail.com>
2024-04-21 17:45:33 +0200

    funcion fecha y hora

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let fechaHora = document.getElementById("fechaHora")
+
+function actualizarHora(){
+    let ahora = new Date();
+    let ejemplo = { year:"numeric",month:"long", day:"numeric", hour:"numeric", minute:"numeric",second:"numeric",hour12:false}
+    fechaHora.textContent = ahora.toLocaleDateString("es-ES",ejemplo)
+}
+actualizarHora();
+
+setInterval(actualizarHora,1000);
